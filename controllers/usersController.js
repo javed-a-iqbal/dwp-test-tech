dfbd232be51d798b761af75ad0bbd8179afec5ad
Javed Iqbal <javedzabi@hotmail.com>
2021-02-21 14:21:15 +0000

    Get people with in 50 miles radius or in the london city, logging support by winston added

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,18 +2,46 @@ const axios = require('axios');
 const geolib = require('geolib');
 const _ = require('lodash');
 const constants = require('../lib/constants');
+require('dotenv').config()
+const successLog = require('../logger/logger').successLog;
+const errorLog = require('../logger/logger').errorLog;
+const auditLog = require('../logger/logger').auditLog;
 
 
+const getAllUsers = () => axios.get(`${process.env.API_URL}/users`);
+const getUsersOfCity = (city) => axios.get(`${process.env.API_URL}/city/${city}/users`);
 
-
-const getUsers = async (req, res) => {
-  res.send("hi welcome ")
+const getDistance = (from, to) => {
+  const distanceInMetres = geolib.getDistance(from, to);
+  return geolib.convertDistance(distanceInMetres, "mi");
 }
 
+const getUsersInGivenDistance = async (city, distance) => {
+  const allUsers = await getAllUsers();
+  return allUsers.data.filter(({latitude, longitude}) => {
+    return getDistance(city, {latitude, longitude}) <= distance;
+  });
+}
 
-
+const getUsers = async (req, res) => {
+  try {
+    const usersByCity = getUsersOfCity(req.params.city);
+    const usersByDistance = getUsersInGivenDistance(constants[req.params.city], 50);
+    
+    const [UsersInLondon, UsersInGivenDistance] = await Promise.all([usersByCity, usersByDistance]);
+    successLog.info("Data retrieved");
+    auditLog.info(`Request has been fulfill for the users with in the london or with in the 50 miles area of london `, { type: "audit", archetype: "CN01", product: "people in London or with in 50 miles", emailId: "xyx@xyz.com",  productType: "api data", "tags": "analytics" });
+    res.status(200).json(_.union(UsersInLondon.data, UsersInGivenDistance));
+  } catch(errors) {
+    errorLog.error("Error during exeution ", { error: errors }); 
+    res.send(errors);
+  }
+}
 
 module.exports = {
-  getUsers
-  
+  getUsers,
+  getUsersOfCity,
+  getAllUsers,
+  getDistance,
+  getUsersInGivenDistance
 }
