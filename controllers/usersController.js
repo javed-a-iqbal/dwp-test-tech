9efe85b5d4b5c5a3358d02db0fd4ef838879c875
Javed Iqbal <javedzabi@hotmail.com>
2021-02-23 20:56:24 +0000

    [DWP-333] code cleaning using standard, Enum added for messages

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,39 +1,40 @@
-const axios = require('axios');
-const geolib = require('geolib');
-const _ = require('lodash');
-const constants = require('../lib/constants');
+const axios = require('axios')
+const geolib = require('geolib')
+const _ = require('lodash')
+const constants = require('../lib/constants')
 require('dotenv').config()
-const successLog = require('../logger/logger').successLog;
-const errorLog = require('../logger/logger').errorLog;
-const auditLog = require('../logger/logger').auditLog;
+const successLog = require('../logger/logger').successLog
+const errorLog = require('../logger/logger').errorLog
+const auditLog = require('../logger/logger').auditLog
+const productMessages = require('../common-enum').productMessages
 
-
-const getAllUsers = () => axios.get(`${process.env.API_URL}/users`);
-const getUsersOfCity = (city) => axios.get(`${process.env.API_URL}/city/${city}/users`);
+const getAllUsers = () => axios.get(`${process.env.API_URL}/users`)
+const getUsersOfCity = (city) => axios.get(`${process.env.API_URL}/city/${city}/users`)
 
 const getDistance = (from, to) => {
-  const distanceInMetres = geolib.getDistance(from, to);
-  return geolib.convertDistance(distanceInMetres, "mi");
+  const distanceInMetres = geolib.getDistance(from, to)
+  return geolib.convertDistance(distanceInMetres, 'mi')
 }
 
 const getUsersInGivenDistance = async (city, distance) => {
-  const allUsers = await getAllUsers();
-  return allUsers.data.filter(({latitude, longitude}) => {
-    return getDistance(city, {latitude, longitude}) <= distance;
-  });
+  const allUsers = await getAllUsers()
+  return allUsers.data.filter(({ latitude, longitude }) => {
+    return getDistance(city, { latitude, longitude }) <= distance
+  })
 }
 
 const getUsers = async (req, res) => {
   try {
-    const usersByCity = getUsersOfCity(req.params.city);
-    const usersByDistance = getUsersInGivenDistance(constants[req.params.city], 50);
-    
-    const [UsersInLondon, UsersInGivenDistance] = await Promise.all([usersByCity, usersByDistance]);
-    auditLog.info(`Request has been fulfill for the users with in the london or with in the 50 miles area of london `, { type: "audit", archetype: "CN01", product: "people in London or with in 50 miles", emailId: process.env.EMAIL_ADDRESS,  productType: "api data", "tags": "analytics" });
-    res.status(200).json(_.union(UsersInLondon.data, UsersInGivenDistance));
-  } catch(errors) {
-    errorLog.error("Error during exeution "); 
-    //res.send(errors);
+    const usersByCity = getUsersOfCity(req.params.city)
+    const usersByDistance = getUsersInGivenDistance(constants[req.params.city], 50)
+
+    const [UsersInLondon, UsersInGivenDistance] = await Promise.all([usersByCity, usersByDistance])
+    auditLog.info(productMessages.RequestData.value)
+    auditLog.info(productMessages.RequestDataLondon.value, { type: 'audit', archetype: 'CN01', product: productMessages.productName.value, emailId: process.env.EMAIL_ADDRESS, productType: 'api data', tags: 'analytics' })
+    res.status(200).json(_.union(UsersInLondon.data, UsersInGivenDistance))
+  } catch (errors) {
+    errorLog.error(productMessages.ErrorMessage.value + errors)
+    res.send(errors)
   }
 }
 
